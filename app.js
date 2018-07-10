fea1ee16f1b778a121240e1d57f6b74471646330
Danielle Boczkowski <38707471+DanielleBK@users.noreply.github.com>
2018-07-10 14:53:27 -0300

    Twitter
    pages

---
+++ app.js
@@ -0,0 +1,46 @@
+
+
+
+
+function post(){                                    //cod.para 'twittar'
+	var text2 = document.getElementById('text').value;
+	var newPost = document.getElementById('newPost');
+	var newP = document.createElement('div');
+	var button = document.getElementById('button');
+
+	newP.setAttribute('class', 'boxPost' );
+	newP.textContent = text.value;
+	newPost.appendChild(newP);
+	document.getElementById('text').value='';
+	document.getElementById('text').focus(text);
+	button.removeEventListener('click', post);
+
+};
+
+function counter_textarea() {   //cod.contador
+  var deal = 140;
+  var total = text.value.length;
+  var residual = deal - total;
+  contactor.textContent = residual;
+
+  if (residual >= 120 && total < residual) {
+    contactor.style.color = 'blue';
+  }  if (residual >= 130) {
+    contactor.style.color = 'green';
+  } if (residual >= 131  ) {
+    contactor.style.color = 'orange';
+  } if (residual <=0 || residual === 0) {
+    contactor.style.color = 'red';
+  }
+	 if (total >=0 || total <= 140){
+   var button = document.getElementById('button');
+	 button.addEventListener('click', post);
+
+	 }
+}
+
+onKeyPressTextMessage = function(){           //cod.para alterar tamanho da textarea//
+	var textArea = event.currentTarget;
+  textArea.style.height = 'auto';
+  textArea.style.height = textArea.scrollHeight + 'px';
+};
